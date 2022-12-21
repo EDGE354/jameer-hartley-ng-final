69529263e1ac85435df0f926ca5ace32e3a12827
EDGE354 <112958863+EDGE354@users.noreply.github.com>
2022-12-20 20:51:58 -0500

    2 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 import { Injectable } from '@angular/core';
+
 /* . . . */
 @Injectable({
   providedIn: 'root'
@@ -11,12 +12,11 @@ export class CartService {
   getShippingPrices() {
     return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
   }
-  
+
   constructor(
     private http: HttpClient
   ) {}
 /* . . . */
-
 
   addToCart(product: Product) {
     this.items.push(product);
